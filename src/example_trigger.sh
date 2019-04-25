1488b2c789e187a7571b796b9aa1b4e5d2472a3d
alen.badrajan <https://alen.badrajan@titanium-soft.com>
2019-04-25 10:10:43 +0300

    upgrading headless chrome

--- src/example_trigger.sh
+++ src/example_trigger.sh
@@ -1,5 +1,5 @@
 curl -X POST \
-  https://kmk1g66zoj.execute-api.us-east-1.amazonaws.com/dev/pdf \
+  https://hrcd959oka.execute-api.us-east-1.amazonaws.com/production-pdf-cluster/pdf \
   -H 'displayHeaderFooter: true' \
   -H 'landscape: false' \
   -H 'headertemplate: false' \
@@ -13,9 +13,9 @@ curl -X POST \
   -H 'printBackground: true' \
   -H 'scale: 1' \
   -H 'x-user-id: 55507' \
-  -H 'x-user-token: 4efb494f-9d2a-4fa1-885d-8f69a88dd869' \
+  -H 'x-user-token: 3aac14d8-d18d-4abd-b982-b9373f0a2d1f' \
   -H 'x-user-systemcode: W4240241236' \
   -H 'x-user-metro: tx-ho' \
   -H 'filename: test10.pdf' \
-  -H 'url: https://www.apartmentdata.io/reports/tx-ho/custom/819/market_survey?s=1&i=1&c=1&h=1&limit=24&hp=1&f=1&fc=1&d=0&ds=1&da=1&df=1&dh=0&dp=2' > tmp
+  -H 'url: https://www.apartmentdata.io/reports/tx-ho/custom/692/market_survey?s=1&i=1&c=1&h=1&limit=13&hp=0&f=1&fc=1&d=0&ds=1&da=1&df=1&dh=1&dp=2' > tmp
  
